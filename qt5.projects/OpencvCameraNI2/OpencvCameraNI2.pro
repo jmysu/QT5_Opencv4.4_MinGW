3c449318481d1f739d005741bc6bb30dafc60dab
Jimmy Su <jmysu@users.noreply.github.com>
2020-10-16 10:59:54 +0800

    Minimum OpenNI2 depth view for Astra Pro

---
+++ qt5.projects/OpencvCameraNI2/OpencvCameraNI2.pro
@@ -0,0 +1,61 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-10-29T09:10:23
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = OpencvCamera
+TEMPLATE = app
+DESTDIR  = ..
+
+CONFIG  += -std=c++11
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which has been marked as deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+#DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+
+SOURCES += \
+    mainni2.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+        mainwindow.h
+
+FORMS += \
+        mainwindow.ui
+
+win32 {
+    DEFINES += OPENCV440
+    contains(DEFINES, "OPENCV440") {
+     warning("Opencv440")
+     INCLUDEPATH += D:\opencv440\install\include
+     LIBS += -LD:\opencv440\install\x64\mingw\bin \
+        -lopencv_core440 \
+        -lopencv_highgui440 \
+        -lopencv_imgproc440 \
+        -lopencv_features2d440 \
+        -lopencv_calib3d440 \
+        -lopencv_imgcodecs440 \
+        -lopencv_video440 \
+        -lopencv_videoio440
+
+     INCLUDEPATH +=  D:\OpenNI2\Include
+     LIBS += -LD:\OpenNI2\Lib \
+        -lopenni2
+     }
+}
+
+DISTFILES += \
+    main.txt
---
+++ qt5.projects/OpencvCameraNI2/mainni2.cpp
@@ -0,0 +1,102 @@
+﻿// Sample_Depth_UVC_Viewer.cpp : 隅砱諷秶怢茼蚚最唗腔諳萸﹝
+//
+
+//#include "stdafx.h"
+
+#include "OpenNI.h"
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+// OpenCV Header
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+using namespace std;
+using namespace cv;
+
+using namespace openni;
+
+int main(int argc, char* argv[])
+{
+    VideoFrameRef oniDepthImg;
+
+    OpenNI::shutdown();
+
+    Status result = STATUS_OK;
+    result = OpenNI::initialize();
+
+    // open device
+    Device device;
+    result = device.open( openni::ANY_DEVICE );
+
+
+
+    //// create depth stream
+    VideoStream oniDepthStream;
+    result = oniDepthStream.create( device, openni::SENSOR_DEPTH );
+
+    // set depth video mode
+    VideoMode modeDepth;
+    modeDepth.setResolution( 640, 480 );
+    modeDepth.setFps( 60 );
+    modeDepth.setPixelFormat( PIXEL_FORMAT_DEPTH_1_MM );
+    oniDepthStream.setVideoMode(modeDepth);
+    //oniDepthStream.setMirroringEnabled(true);
+    device.setImageRegistrationMode(ImageRegistrationMode::IMAGE_REGISTRATION_DEPTH_TO_COLOR);
+
+    VideoCapture cap;
+    cap.open(1); //Astra on USB list #1
+    if(!cap.isOpened())
+        return false;
+
+    //cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    //cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+    cap.set(CAP_PROP_FRAME_WIDTH,640); //Opencv4
+    cap.set(CAP_PROP_FRAME_HEIGHT,480);//Opencv4
+
+    // start depth stream
+    result = oniDepthStream.start();
+    while( true )
+    {
+
+        Mat frame,rgb;
+        cap>>frame;
+
+        if(frame.empty())
+            break;
+        //cvtColor(frame,rgb,CV_RGB2GRAY);
+        cvtColor(frame,rgb, COLOR_RGB2GRAY);//Opencv4
+
+
+        if( oniDepthStream.readFrame( &oniDepthImg ) == STATUS_OK )
+        {
+
+            cv::Mat cDepthImg( oniDepthImg.getHeight(), oniDepthImg.getWidth(),
+                CV_16UC1, (void*)oniDepthImg.getData() );
+
+            cv::Mat c8BitDepth,c24Bit;
+            cDepthImg.convertTo( c8BitDepth, CV_8U, 255.0 / (8000) );
+
+            Mat dst;
+            addWeighted(c8BitDepth,0.5,rgb,0.5,0,dst);
+            cv::imshow( "Orbbec", dst );
+
+        }
+        waitKey(10);
+    }
+
+    oniDepthStream.destroy();
+
+
+
+    device.close();
+    OpenNI::shutdown();
+    //return 0;
+}
+
