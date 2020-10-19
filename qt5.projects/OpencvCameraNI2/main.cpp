bc5b3f7f7eba78d31feb20629415a309eecb3bc5
Jimmy Su <jmysu@users.noreply.github.com>
2020-10-19 15:58:34 +0800

    minimum clearup

--- qt5.projects/OpencvCameraNI2/main.cpp
+++ qt5.projects/OpencvCameraNI2/main.cpp
@@ -1,8 +1,4 @@
-﻿// Sample_Depth_UVC_Viewer.cpp : 隅砱諷秶怢茼蚚最唗腔諳萸﹝
-//
-
-//#include "stdafx.h"
-
+﻿
 #include "OpenNI.h"
 
 
@@ -15,15 +11,50 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+//#include <opencv2/calib3d/calib3d.hpp>
 
 using namespace std;
 using namespace cv;
 
 using namespace openni;
 
+
+/*
+ * Depth Max/Min adjust
+ *
+ */
+cv::Mat adjustDepth(const cv::Mat& inImage)
+{
+    // from https://orbbec3d.com/product-astra/
+    // Astra S has a depth in the range 0.35m to 2.5m
+    //int maxDepth = 2500;
+    //int minDepth = 350; // in mm
+    // Astra Pro has a depth in the range 0.6m to 8m
+    int maxDepth = 3000;
+    int minDepth = 600; // in mm
+
+    cv::Mat retImage = inImage;
+
+    for(int j = 0; j < retImage.rows; j++)
+        for(int i = 0; i < retImage.cols; i++) {
+            if (retImage.at<ushort>(j, i) < maxDepth) //convert points in range
+                retImage.at<ushort>(j, i) = maxDepth - (retImage.at<ushort>(j, i) - minDepth);
+            else
+                retImage.at<ushort>(j, i) = 10; //dark it
+            }
+
+        return retImage;
+}
+
+
+
+
 int main(int argc, char* argv[])
 {
+    auto version = openni::OpenNI::getVersion();
+    std::cout << "OpenNI " << version.major << "." << version.minor << "." <<
+                               version.maintenance << "." << version.build << std::endl;
+
     VideoFrameRef oniDepthImg;
 
     OpenNI::shutdown();
@@ -36,57 +67,63 @@ int main(int argc, char* argv[])
     result = device.open( openni::ANY_DEVICE );
 
 
-
     //// create depth stream
     VideoStream oniDepthStream;
     result = oniDepthStream.create( device, openni::SENSOR_DEPTH );
-    //result = oniDepthStream.create( device, openni::SENSOR_COLOR );
 
     // set depth video mode
     VideoMode modeDepth;
     modeDepth.setResolution( 640, 480 );
-    modeDepth.setFps( 60 );
+    modeDepth.setFps( 30 );
     modeDepth.setPixelFormat( PIXEL_FORMAT_DEPTH_1_MM );
     oniDepthStream.setVideoMode(modeDepth);
     //oniDepthStream.setMirroringEnabled(true);
     device.setImageRegistrationMode(ImageRegistrationMode::IMAGE_REGISTRATION_DEPTH_TO_COLOR);
 
+    /*
+    //Set UVC camera capture
     VideoCapture cap;
     cap.open(1); //Astra on USB list #1
     if(!cap.isOpened())
         return false;
-
-    //cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
-    //cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-    cap.set(CAP_PROP_FRAME_WIDTH,640); //Opencv4
+    cap.set(CAP_PROP_FRAME_WIDTH,720); //Opencv4
     cap.set(CAP_PROP_FRAME_HEIGHT,480);//Opencv4
+    */
+
 
     // start depth stream
     result = oniDepthStream.start();
     while( true ) {
 
-        Mat frame,rgb;
-        cap>>frame;
-
-        if(frame.empty())
-            break;
+        /*
+        //Capture UVC
+        //Mat frame,rgb;
+        //cap>>frame;
+        //if(frame.empty())
+        //    break;
         //cvtColor(frame,rgb,CV_RGB2GRAY);
-        cvtColor(frame,rgb, COLOR_RGB2GRAY);//Opencv4
-
+        //cvtColor(frame,rgb, COLOR_RGB2GRAY);//Opencv4
+        */
 
         if ( oniDepthStream.readFrame( &oniDepthImg ) == STATUS_OK ) {
-
             cv::Mat cDepthImg( oniDepthImg.getHeight(), oniDepthImg.getWidth(),
                 CV_16UC1, (void*)oniDepthImg.getData() );
 
-            cv::Mat c8BitDepth,c24Bit;
-            cDepthImg.convertTo( c8BitDepth, CV_8U, 255.0 / (1000) );
+            //cv::Mat c8BitDepth,c24Bit;
+            //cDepthImg.convertTo( c8BitDepth, CV_8U, 255.0 / (1000) );
 
-            Mat dst;
-            addWeighted(c8BitDepth,0.5,rgb,0.5,0,dst);
-            cv::imshow( "Astra Depth", dst );
+            //Mat dst;
+            //addWeighted(c8BitDepth,0.5,rgb,0.5,0,dst);
+            //cv::imshow( "Astra Depth", dst );
+            //cv::imshow( "Astra Depth", cDepthImg);
 
+
+             cv::Mat adjustedDepth = adjustDepth(cDepthImg);
+             cv::Mat dispImage;
+             adjustedDepth.convertTo(dispImage, CV_8UC1, 255.0f/4000); //Darker
+             cv::imshow("Adjusted Depth", dispImage);
             }
+
         waitKey(10);
         }
 
@@ -96,4 +133,3 @@ int main(int argc, char* argv[])
     OpenNI::shutdown();
     //return 0;
 }
-
